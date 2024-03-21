225d5e62704ceb11fed7edc1b8c616e5ba37220a
Jacob Friis-Hasché <93619265+JacobFriish@users.noreply.github.com>
2024-03-21 21:48:04 +0100

    Lavede opgaver

--- Jacob/InaurguralJacob.py
+++ Jacob/InaurguralJacob.py
@@ -1,3 +1,15 @@
+# Importerer packacges
+
+from scipy.optimize import minimize
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+
+# DEFINERER PARAMETRER
+alpha = 1/3
+beta = 2/3
+N = 75
+p1_values = np.linspace(0.5, 2.5, N)
 
 
 # DEFINERER NYTTEFUNKTIONER
@@ -42,3 +54,10 @@ def market_clearing_condition_x2(p1):
     total_demand_x2 = demand_A_x2(p1, omega_A1, omega_A2, alpha) + demand_B_x2(p1, omega_B1, omega_B2, beta)
     total_endowment_x2 = omega_A2 + omega_B2
     return total_demand_x2 - total_endowment_x2
+
+
+
+def allocation_at_price(p1, alpha, omega_A1, omega_A2):
+    allocation_x1 = alpha * (p1 * omega_A1 + omega_A2) / p1
+    allocation_x2 = (1 - alpha) * (p1 * omega_A1 + omega_A2)
+    return allocation_x1, allocation_x2
